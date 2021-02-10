2d19430132a755004d83a53fee63e111cc7f88e0
luthfisauqi17 <40749167+luthfisauqi17@users.noreply.github.com>
2021-02-10 23:06:10 +0700

    Give comments on Login Activity

--- app/src/main/java/com/example/storee/LoginActivity.java
+++ app/src/main/java/com/example/storee/LoginActivity.java
@@ -37,44 +37,113 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+
+    /* This function used for Check connection, take the login inputs from user and
+    pass it into the API to check whether the user is listed in database or not */
     public void login(View v) {
+
+        // This variable is for storing the email input from user
         String email = ((EditText) findViewById(R.id.emailEditText)).getText().toString();
+        // This variable is for storing the password input from user
         String password = ((EditText) findViewById(R.id.passwordEditText)).getText().toString();
-        TextView loginStatsTV = ((TextView)findViewById(R.id.loginStatusTV));
 
-        RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
-        String url = "https://storee-api.000webhostapp.com/public/user/login";
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Toast.makeText(LoginActivity.this, "Login SUCCESS", Toast.LENGTH_SHORT).show();
-                loginStatsTV.setTextColor(Color.parseColor("#48A868"));
-                loginStatsTV.setText("Login SUCCESS");
-                Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(i);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(LoginActivity.this, "Login FAILED", Toast.LENGTH_SHORT).show();
-                loginStatsTV.setTextColor(Color.parseColor("#F19696"));
-                loginStatsTV.setText("Sorry, invalid user or password");
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("email", email);
-                params.put("password", password);
-                return params;
-            }
+        // These variables needed for storing the components data from the UI
+        TextView loginStatsTV = ((TextView) findViewById(R.id.loginStatusTV));
+        EditText emailET = ((EditText) findViewById(R.id.emailEditText));
+        EditText passwordET = ((EditText) findViewById(R.id.passwordEditText));
 
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("Content-Type", "application/x-www-form-urlencoded");
-                return params;
-            }
-        };
-        requestQueue.add(stringRequest);
+        // This variable is for storing the error messages
+        String errorString = "";
+        // This variable indicates if the user inputs have any error or not
+        boolean errorExist = false;
+
+        // First of all, we set the all inputs background to netral type of background
+        emailET.setBackgroundResource(R.drawable.input_box_netral);
+        passwordET.setBackgroundResource(R.drawable.input_box_netral);
+
+        // Check if the email input is empty or not
+        if (email.matches("")) {
+            // Set the email input field background to warning type of background
+            emailET.setBackgroundResource(R.drawable.input_box_warning);
+            // Append the error message to the errorString variable
+            errorString += "*Email input must not empty!\n";
+            // Set the errorExist boolean to true
+            errorExist = true;
+        }
+
+        // Check if the password input is empty or not
+        if (password.matches("")) {
+            // Set the password input field background to warning type of background
+            passwordET.setBackgroundResource(R.drawable.input_box_warning);
+            // Append the error message to the errorString variable
+            errorString += "*Password input must not empty!\n";
+            // Set the errorExist boolean to true
+            errorExist = true;
+        }
+
+        // We need to check if we have error or not, if yes, we cannot process to request API
+        if (!errorExist) {
+            // We need to make a RequestQueue object for passing the Request to the API
+            RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+            // This is the URL of the API
+            String url = "https://storee-api.000webhostapp.com/public/user/login";
+            // A canned request for retrieving the response body at a given URL as a String
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                // This is the listener for the Success response without any errors
+                @Override
+                public void onResponse(String response) {
+                    // Create a popup toast to inform the user
+                    Toast.makeText(LoginActivity.this, "Login SUCCEED", Toast.LENGTH_SHORT).show();
+                    // Set the color of the success message
+                    loginStatsTV.setTextColor(Color.parseColor("#48A868"));
+                    // Set the text to "Login SUCCEED"
+                    loginStatsTV.setText("Login SUCCEED");
+                    // Create an Intent to move to another activity
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    // Move to another activity
+                    startActivity(i);
+                }
+            }, new Response.ErrorListener() {
+                // This is the listener if there is any errors when receiving the response
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // Create a popup toast to inform the user
+                    Toast.makeText(LoginActivity.this, "Login FAILED", Toast.LENGTH_SHORT).show();
+                    // Set the color of the fail message
+                    loginStatsTV.setTextColor(Color.parseColor("#F19696"));
+                    // Set the text to "Login failed"
+                    loginStatsTV.setText("Login failed, please try again");
+                }
+            }) {
+                @Override
+                // Overriding the getParams method allows you to build the HashMap and
+                // return the object to the Volley request for posting.
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("email", email);
+                    params.put("password", password);
+                    return params;
+                }
+
+                // Returns a list of extra HTTP headers to go along with this request
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    // application/x-www-form-urlencoded represents a URL encoded form.
+                    // This is the default value if enctype attribute is not set to anything.
+                    params.put("Content-Type", "application/x-www-form-urlencoded");
+                    return params;
+                }
+            };
+            // Add a request
+            requestQueue.add(stringRequest);
+        }
+        // This means we have some errors, and we need to let the user know what the error are
+        else {
+            // Set the text color to reddish color
+            loginStatsTV.setTextColor(Color.parseColor("#F19696"));
+            // Display all errors messages
+            loginStatsTV.setText(errorString);
+        }
     }
 }
--- app/src/main/java/com/example/storee/RegisterActivity.java
+++ app/src/main/java/com/example/storee/RegisterActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,52 +33,121 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void register(View v) {
-        String firstName = ((TextView)findViewById(R.id.firstnameET)).getText().toString();
-        String lastName = ((TextView)findViewById(R.id.lastnameET)).getText().toString();
-        String email = ((TextView)findViewById(R.id.emailET)).getText().toString();
-        String phoneNumber = ((TextView)findViewById(R.id.phoneET)).getText().toString();
-        String password = ((TextView)findViewById(R.id.passwordET)).getText().toString();
-        String confirmPassword = ((TextView)findViewById(R.id.confirmPasswordET)).getText().toString();
+//        Variables
+        String firstName = ((EditText)findViewById(R.id.firstnameET)).getText().toString();
+        String lastName = ((EditText)findViewById(R.id.lastnameET)).getText().toString();
+        String email = ((EditText)findViewById(R.id.emailET)).getText().toString();
+        String phoneNumber = ((EditText)findViewById(R.id.phoneET)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordET)).getText().toString();
+        String confirmPassword = ((EditText)findViewById(R.id.confirmPasswordET)).getText().toString();
+
         TextView registerStatsTV = findViewById(R.id.registerStatsTV);
+        EditText firstNameET = ((EditText)findViewById(R.id.firstnameET));
+        EditText lastNameET = ((EditText)findViewById(R.id.lastnameET));
+        EditText emailET = ((EditText)findViewById(R.id.emailET));
+        EditText phoneET = ((EditText)findViewById(R.id.phoneET));
+        EditText passwordET = ((EditText)findViewById(R.id.passwordET));
+        EditText confirmPasswordET = ((EditText)findViewById(R.id.confirmPasswordET));
+        CheckBox termAgreementCB = ((CheckBox)findViewById(R.id.agreementCB));
 
-        RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
-        String url = "https://storee-api.000webhostapp.com/public/user/register";
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Toast.makeText(RegisterActivity.this, "Register SUCCESS", Toast.LENGTH_SHORT).show();
-                registerStatsTV.setTextColor(Color.parseColor("#48A868"));
-                registerStatsTV.setText("Login SUCCESS");
-                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(i);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(RegisterActivity.this, "Login FAILED", Toast.LENGTH_SHORT).show();
-                registerStatsTV.setTextColor(Color.parseColor("#F19696"));
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("user_first_name", firstName);
-                params.put("user_last_name", lastName);
-                params.put("user_email", email);
-                params.put("user_pn", phoneNumber);
-                params.put("user_password", password);
-                return params;
-            }
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("Content-Type", "application/x-www-form-urlencoded");
-                return params;
-            }
-        };
-        requestQueue.add(stringRequest);
-    }
+        String errorString = "";
+        boolean errorExist = false;
 
-    public boolean confirmPassword(String password, String confirmPassword) {
-        return true;
+        firstNameET.setBackgroundResource(R.drawable.input_box_netral);
+        lastNameET.setBackgroundResource(R.drawable.input_box_netral);
+        emailET.setBackgroundResource(R.drawable.input_box_netral);
+        phoneET.setBackgroundResource(R.drawable.input_box_netral);
+        passwordET.setBackgroundResource(R.drawable.input_box_netral);
+        confirmPasswordET.setBackgroundResource(R.drawable.input_box_netral);
+
+
+        if(!termAgreementCB.isChecked()) {
+            errorString += "*Please check the agreement!\n";
+            errorExist = true;
+        }
+
+        if(firstName.matches("")) {
+            firstNameET.setBackgroundResource(R.drawable.input_box_warning);
+            errorString += "*First name input must not empty!\n";
+            errorExist = true;
+        }
+
+        if(lastName.matches("")) {
+            lastNameET.setBackgroundResource(R.drawable.input_box_warning);
+            errorString += "*Last name input must not empty!\n";
+            errorExist = true;
+        }
+
+        if(email.matches("")) {
+            emailET.setBackgroundResource(R.drawable.input_box_warning);
+            errorString += "*Email input must not empty!\n";
+            errorExist = true;
+        }
+
+        if(phoneNumber.matches("")) {
+            phoneET.setBackgroundResource(R.drawable.input_box_warning);
+            errorString += "*Phone number input must not empty!\n";
+            errorExist = true;
+        }
+
+        if(password.matches("")) {
+            passwordET.setBackgroundResource(R.drawable.input_box_warning);
+            errorString += "*Password input must not empty!\n";
+            errorExist = true;
+        }
+        else {
+            if(!password.matches(confirmPassword)) {
+                passwordET.setBackgroundResource(R.drawable.input_box_warning);
+                confirmPasswordET.setBackgroundResource(R.drawable.input_box_warning);
+                errorString += "*Password is not match with the confirmation password!\n";
+                errorExist = true;
+            }
+        }
+
+        if(!errorExist) {
+//        Request API
+            RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+            String url = "https://storee-api.000webhostapp.com/public/user/register";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Toast.makeText(RegisterActivity.this, "Register SUCCEED", Toast.LENGTH_SHORT).show();
+                    registerStatsTV.setTextColor(Color.parseColor("#48A868"));
+                    registerStatsTV.setText("Register SUCCEED");
+                    Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(i);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(RegisterActivity.this, "Register FAILED", Toast.LENGTH_SHORT).show();
+                    registerStatsTV.setTextColor(Color.parseColor("#F19696"));
+                    registerStatsTV.setText("Register failed, please try again");
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("user_first_name", firstName);
+                    params.put("user_last_name", lastName);
+                    params.put("user_email", email);
+                    params.put("user_pn", phoneNumber);
+                    params.put("user_password", password);
+                    return params;
+                }
+
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Content-Type", "application/x-www-form-urlencoded");
+                    return params;
+                }
+            };
+            requestQueue.add(stringRequest);
+        }
+        else
+        {
+            registerStatsTV.setTextColor(Color.parseColor("#F19696"));
+            registerStatsTV.setText(errorString);
+        }
     }
 }
