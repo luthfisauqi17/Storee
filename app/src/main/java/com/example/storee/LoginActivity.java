4dcbbe83394724b7c45a8acb6cc326c6a6cc7a8a
luthfisauqi17 <40749167+luthfisauqi17@users.noreply.github.com>
2021-02-16 15:27:09 +0700

    Remove volley from dependency

--- app/src/main/java/com/example/storee/LoginActivity.java
+++ app/src/main/java/com/example/storee/LoginActivity.java
@@ -11,14 +11,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
